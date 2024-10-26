0f7e0809fcae6127601c7a762fac9def05b8c452
Feras Alkilani <eng.feras90@gmail.com>
2024-10-26 20:36:15 +0200

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+  console.log("Server listening on port 3000");
 });
