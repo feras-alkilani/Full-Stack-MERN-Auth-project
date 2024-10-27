d10bb4802d8b4b75b6bb2d2609188a17a5335903
Feras Alkilani <eng.feras90@gmail.com>
2024-10-27 15:10:03 +0100

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 50000 // تحديد المهلة بـ 5 ثوانٍ
+  })
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -19,6 +24,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/user", userRoutes); // Moved above app.listen
+app.use("/api/auth", authRoutes); // Moved above app.listen
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,16 @@
-import express from "express";
-import { test } from "../controllers/user.controller.js";
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
-const router = express.Router();
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
 
-router.get("/", test);
-// router.post("/update/:id", verifyToken, updateUser);
-// router.delete("/delete/:id", verifyToken, deleteUser);
-
-export default router;
+export default signup;
